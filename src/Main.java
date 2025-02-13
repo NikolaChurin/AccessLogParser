cbc69e646ea44381712b8b747d3e2e724707344e
Nikolay Churin <nchurin@inno.tech>
2025-02-13 22:54:49 +0300

    Solution

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int count = 0;
-
+        System.out.println("Введите путь к файлу");
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
@@ -23,22 +23,39 @@ public class Main {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader = new BufferedReader(fileReader);
                 String line;
-                int min = 0;
-                int max = 0;
                 int countLine = 0;
+                int countGoogleBot = 0;
+                int countYandexBot = 0;
 
                 while ((line = reader.readLine()) != null) {
                     int length = line.length();
                     if (length >= 1024) {
                         throw new IncorrectSizeException("Error. В строке более 1024 символа");
                     }
-                    if(min==0){min = length;}
-                    min = Math.min(length, min);
-                    max = Math.max(length, max);
                     countLine++;
+
+                    String[] parsLine = line.split(";");
+                    int beginIndex = line.indexOf('(');
+                    int endIndex = line.indexOf(')', beginIndex);
+                    String subLine = line.substring(beginIndex + 1, endIndex).split(";")[1];
+                    if (!subLine.contains("/")) {
+                        continue;
+                    }
+                    String userAgent = subLine.substring(0, subLine.indexOf("/")).trim();
+                    switch (userAgent) {
+                        case "Googlebot":
+                            countGoogleBot++;
+                            break;
+                        case "YandexBot":
+                            countYandexBot++;
+                            break;
+                    }
                 }
-                System.out.printf("Самая коротка строка %d символов, самая длинная строка %d символов", min, max);
+                System.out.println(String.format("В файле присутствует %d%% обращений Googlebot\n" +
+                        "В файле присутствует %d%% обращений YandexBot", countGoogleBot*100 / countLine, countYandexBot*100 / countLine));
                 System.out.printf("Количество строк = " + countLine);
+
+
             } catch (IOException ex) {
                 System.out.println(ex.getMessage());
                 ex.printStackTrace();
